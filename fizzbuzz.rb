0512d083fdde4474cb3dce9febf33645000f8e62
aashnaag <aashna.agarwal55@gmail.com>
2018-07-24 18:30:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 
 
 def fizzbuzz(number)
-  if % 15 = 0
+  if % 15 == 0
     returns "FizzBuzz"
-  elsif % 5 = 0
+  elsif % 5 == 0
     returns "Buzz"
-  elsif % 3 = 0
+  elsif % 3 == 0
     returns "Fizz"
   end
 end
